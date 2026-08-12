e983fb9ea8c35d9eb246a2b85a666ecd40b2ed11
mtecfix <116046821+mtecfix@users.noreply.github.com>
2026-08-11 21:45:19 -0400

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version: 5.9
+import PackageDescription
+let package = Package(
+    name: "ContractorApp",
+    platforms: [.iOS(.v16)],
+    products: [.library(name: "ContractorApp", targets: ["ContractorApp"])],
+    targets: [
+        .target(name: "ContractorApp", dependencies: []),
+        .testTarget(name: "ContractorAppTests", dependencies: ["ContractorApp"])
+    ]
+)
