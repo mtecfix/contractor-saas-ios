d43bac3d4c12ab9b1043b5260953543a2e4f5a20
mtecfix <116046821+mtecfix@users.noreply.github.com>
2026-08-11 21:45:53 -0400

    Add source files

---
+++ ContractorApp/Config.swift
@@ -0,0 +1,8 @@
+import Foundation
+enum Config {
+    static let apiEndpoint   = "https://8rd8xrv4ef.execute-api.us-east-1.amazonaws.com/dev"
+    static let cognitoPoolId = "us-east-1_0X50w7Bkj"
+    static let cognitoClient = "3pk8iotqpq0pjdjfcgof8tric0"
+    static let awsRegion     = "us-east-1"
+    static let s3Bucket      = "contractor-663877906756"
+}
