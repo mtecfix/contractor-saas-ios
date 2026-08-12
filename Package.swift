3d8ef17568d508be01248eb7904f8b4e476efdc0
mtecfix <116046821+mtecfix@users.noreply.github.com>
2026-08-12 12:12:20 -0400

    iOS only platform, remove macOS

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "ContractorApp",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(name: "ContractorApp", targets: ["ContractorApp"])
     ],
